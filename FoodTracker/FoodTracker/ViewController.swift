7cf0d422200786e0239f7f46bcd83fc33e695ea3
TTOzzi <devttozzi@gmail.com>
2020-03-03 16:51:13 +0900

    프로젝트 생성

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by TTOzzi on 2020/03/03.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
