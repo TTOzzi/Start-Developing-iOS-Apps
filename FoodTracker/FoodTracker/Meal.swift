1d6a7e20dd17ab8c250f8ed7bed84fca268fe951
TTOzzi <devttozzi@gmail.com>
2020-03-05 14:10:50 +0900

    Define Data Model

---
+++ FoodTracker/FoodTracker/Meal.swift
@@ -0,0 +1,28 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by TTOzzi on 2020/03/05.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        guard !name.isEmpty else { return nil }
+        guard rating >= 0 && rating <= 5 else { return nil }
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
